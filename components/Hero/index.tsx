841f5ecb7c1c0e339e8d993b16c5f4ec25b22f66
hope-k <hopekumordzie@gmail.com>
2022-12-31 19:53:48 +0000

    minor ui changesv2

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -8,10 +8,10 @@ import { PageInfo } from '../../typings'
 import { urlFor } from '../../sanity'
 
 type Props = {
-pageInfo: PageInfo
+    pageInfo: PageInfo
 }
 
-const Hero = ({pageInfo}: Props) => {
+const Hero = ({ pageInfo }: Props) => {
     const [text, count] = useTypewriter({
         words: [`Hi My Name Is ${pageInfo?.name}`, 'Fullstack Developer.tsx', "< ExplorePortfolio />"],
         loop: true,
@@ -46,52 +46,55 @@ const Hero = ({pageInfo}: Props) => {
 
     }
     return (
-        <section id='hero' className=' snap-center h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden relative  '>
+        <motion.section layout='size' id='hero' className='px-10 snap-center h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden relative  '>
             <div className='heroBg'></div>
             <BackgroundCircle />
-            <div className=' h-28 w-28 relative '>
+            <motion.div layout='position' className=' h-28 w-28 relative '>
                 <Image width={128} height={128} alt='Hope-K Photo' src={urlFor(pageInfo?.heroImage)} className='rounded-full mx-auto' />
-            </div>
-            <motion.div className='z-20 '>
-                <h2 className=' font-light text-sm uppercase text-gray-500 pb-2 tracking-[12px] md:tracking-[14px] px-5 whitespace-nowrap'>
-                    {pageInfo?.role}
-                </h2>
-                <motion.div layout='position'  className='text-5xl lg:text-6xl font-semibold px-10 '>
-                    <motion.span layout className='mr-3 '>{text}</motion.span>
+            </motion.div>
+            <motion.h2 layout className='z-20 relative font-light text-sm uppercase text-gray-500 pb-2 tracking-[12px] md:tracking-[14px] px-5 whitespace-nowrap'>
+                {pageInfo?.role}
+            </motion.h2>
+
+            <motion.div layout='position' className='z-20 relative'>
+                <motion.div className='text-5xl lg:text-6xl font-semibold px-10 '>
+                    <motion.span className='mr-3 '>{text}</motion.span>
                     <Cursor cursorColor='teal' />
                 </motion.div>
-                <motion.div
-                    variants={{
-                        show: {
-                            opacity: 1,
-                            transition: {
-                                staggerChildren: 0.15,
-                                duration: 1.8,
-                                type: 'spring',
-
-                            }
-                        }
-                    }}
-                    initial='hidden'
-                    whileInView='show'
-                    exit='exit'
-                    className='pt-5 '
-                >
-                    <Link href='#about'>
-                        <motion.button variants={buttonVariant} className='hero-button'>About</motion.button>
-                    </Link>
-                    <Link href='#contact'>
-                        <motion.button variants={buttonVariant} className='hero-button'>Contact</motion.button>
-                    </Link>
-                    <Link href='#skills'>
-                        <motion.button variants={buttonVariant} className='hero-button'>Skills</motion.button>
-                    </Link>
-                    <Link href='#projects'>
-                        <motion.button variants={buttonVariant} className='hero-button'>Projects</motion.button>
-                    </Link>
-                </motion.div>
             </motion.div>
-        </section>
+
+            <motion.div
+                variants={{
+                    show: {
+                        opacity: 1,
+                        transition: {
+                            staggerChildren: 0.15,
+                            duration: 1.8,
+                            type: 'spring',
+
+                        }
+                    }
+                }}
+                initial='hidden'
+                whileInView='show'
+                exit='exit'
+                className='z-20 relative mt-5  '
+                layout
+            >
+                <Link href='#about'>
+                    <motion.button variants={buttonVariant} className='hero-button'>About</motion.button>
+                </Link>
+                <Link href='#contact'>
+                    <motion.button variants={buttonVariant} className='hero-button'>Contact</motion.button>
+                </Link>
+                <Link href='#skills'>
+                    <motion.button variants={buttonVariant} className='hero-button'>Skills</motion.button>
+                </Link>
+                <Link href='#projects'>
+                    <motion.button variants={buttonVariant} className='hero-button'>Projects</motion.button>
+                </Link>
+            </motion.div>
+        </motion.section>
     )
 }
 
