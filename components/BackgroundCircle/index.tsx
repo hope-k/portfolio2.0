e0a5c2b00d42dac9de7e221d2f483528a8009ce6
hope-k <hopekumordzie@gmail.com>
2023-07-24 14:17:04 +0000

    minor ui changess

--- components/BackgroundCircle/index.tsx
+++ components/BackgroundCircle/index.tsx
@@ -10,7 +10,7 @@ const BackgroundCircle = ({ }: Props) => {
             }}
             whileInView={{
                 scale: [1, .6, .8, 1.8, 1],
-                opacity: [0.07, 0.12, 0.16, 0.18, 0.20, 0.22],
+                opacity: [0.09, 0.14, 0.18, 0.19, 0.22, 0.27],
             }}
             transition={{
                 duration: 10,
@@ -23,7 +23,7 @@ const BackgroundCircle = ({ }: Props) => {
             }}
             className='relative flex justify-center items-center'
         >
-            <div className='absolute border border-[#F7AB0A]/30 rounded-full w-[200px] h-[200px] animate-ping mt-52 ' />
+            <div className='absolute border border-[#F7AB0A]/50 rounded-full w-[200px] h-[200px] animate-ping mt-52 ' />
             <div className='absolute border border-[#333333] rounded-full w-[300px] h-[300px] mt-52 ' />
             <div className='absolute border border-[#333333] rounded-full w-[500px] h-[500px] mt-52 ' />
             <div className=' border border-[teal] rounded-full opacity-20 h-[650px] w-[650px] mt-52 absolute animate-pulse ' />
